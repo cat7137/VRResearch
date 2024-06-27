18f5566a0725ad1f731c168daeabba1e1fba6908
cat7137 <98343508+cat7137@users.noreply.github.com>
2024-06-27 18:47:35 -0400

    trying to figure out data collection

--- Assets/Scripts/EyeDataCollection.cs
+++ Assets/Scripts/EyeDataCollection.cs
@@ -4,51 +4,106 @@ using UnityEngine;
 using UnityEngine.XR;
 using System.IO;
 using System.IO.Enumeration;
+using Tobii.Research;
+using Tobii.Research.Unity;
+
 
 public class EyeDataCollection : MonoBehaviour
 {
-    private Transform leftEyeTransform;
-    private Transform rightEyeTransform;
-    private Transform fixationPointTransform;
-    private Vector3 leftEyePosition;
-    private Vector3 rightEyePosition;
-    private Vector3 fixationPoint;
-    private Eyes eyes;
+    
+   
     private string filePath;
     private StreamWriter writer;
+    private static List<string> eyeData;
+    private EyeTrackerCollection eyeTrackers;
     
+
+
     // Start is called before the first frame update
     void Start()
     {
         filePath = Path.Combine(Application.dataPath, "EyeTrackingData.txt");
-        writer = new StreamWriter(filePath, true);
-        writer.WriteLine("LeftEyePosition,RightEyePosition,FixationPoint");
-        Debug.Log("DataWriter initialized. File Path:" + filePath);
+        writer = new StreamWriter(filePath);
+        writer.WriteLine("LeftEyeGazePosition,RightEyeGazePosition");
+        eyeData = new List<string>();
+        StartCoroutine(InitializeEyeTracker());
+    }
+
+    IEnumerator InitializeEyeTracker()
+    {
+        
+
+        // Retry until an eye tracker is found
+        do
+        {
+            eyeTrackers = EyeTrackingOperations.FindAllEyeTrackers();
+            
+            yield return new WaitForSeconds(1f); // Wait for 1 second before retrying
+        }
+        while (eyeTrackers.Count == 0);
+
+        // Assuming there's only one eye tracker, or you choose the first one
+        foreach (var eyeTracker in eyeTrackers)
+        {
+
+            if (eyeTracker == null)
+            {
+                Debug.LogError("Failed to initialize eye tracker.");
+                yield break;
+            }
+            else { 
+            Debug.Log($"Initialized eye tracker: {eyeTracker.Address}");
+
+                // Subscribe to gaze data received event
+                if (eyeTracker != null)
+                {
+                    eyeTracker.HMDGazeDataReceived += EyeTracker_HMDGazeDataReceived;
+                   
+                    Debug.Log("Should be subscribed");
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(eyes.TryGetLeftEyePosition(out leftEyePosition))
-        {
-            leftEyeTransform.localPosition = leftEyePosition;
-        }
-        if(eyes.TryGetRightEyePosition(out rightEyePosition))
-        {
-            rightEyeTransform.localPosition = rightEyePosition;
-        }
-        if(eyes.TryGetFixationPoint(out fixationPoint))
-        {
-            fixationPointTransform.localPosition = fixationPoint;
-        }
-        string dataToWrite = $"{leftEyeTransform},{rightEyeTransform},{fixationPointTransform}";
-        writer.WriteLine(dataToWrite);
+       
+    }
+ 
+    private static void EyeTracker_HMDGazeDataReceived(object sender, HMDGazeDataEventArgs e)
+    {
+
+        Debug.Log(string.Format(
+            "Got HMD gaze data with {0} left eye direction described by unit vector ({1}, {2}, {3}) in the HMD coordinate system.",
+            e.LeftEye.GazeDirection.Validity,
+            e.LeftEye.GazeDirection.UnitVector.X,
+            e.LeftEye.GazeDirection.UnitVector.Y,
+            e.LeftEye.GazeDirection.UnitVector.Z));
+
+           
+            HMDEyeData leftEyeData = e.LeftEye;
+            HMDEyeData rightEyeData = e.RightEye;
+            HMDGazeDirection leftEyeGazeDirection = leftEyeData.GazeDirection;
+            HMDGazeDirection rightEyeGazeDirection = rightEyeData.GazeDirection;
+        
+
+            string dataToWrite = $"{leftEyeGazeDirection.UnitVector},{rightEyeGazeDirection.UnitVector}";
+            eyeData.Add(dataToWrite);
     }
 
-    private void OnDestroy()
+    private void OnApplicationQuit()
     {
+        foreach (var eyeTracker in eyeTrackers)
+        {
+           eyeTracker.HMDGazeDataReceived -= EyeTracker_HMDGazeDataReceived;
+        }
         if (writer!= null)
         {
+            foreach (string data in eyeData)
+            {
+                writer.WriteLine(data);
+            }
             writer.Close();
         }
     }
