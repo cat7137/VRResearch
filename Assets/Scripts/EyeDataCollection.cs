5632253ab59838f263f4f9da0c81a0f31c5c90a3
cat7137 <98343508+cat7137@users.noreply.github.com>
2024-06-28 13:56:43 -0400

    ViveOpenXR Plugin

--- Assets/Scripts/EyeDataCollection.cs
+++ Assets/Scripts/EyeDataCollection.cs
@@ -14,6 +14,8 @@ public class EyeDataCollection : MonoBehaviour
     private string filePath;
     private StreamWriter writer;
     private static List<string> eyeData;
+    private Vector3 gazeDirection;
+    private InputDevice eyeTrackingDevice;
     
     
 
@@ -21,6 +23,18 @@ public class EyeDataCollection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        List<InputDevice> inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.EyeTracking, inputDevices);
+        if(inputDevices.Count > 0)
+        {
+            
+            eyeTrackingDevice = inputDevices[0];
+            Debug.Log("Eye Tracker Initialized");
+        }
+        else
+        {
+            Debug.LogError("No eye Tracking Device Found!");
+        }
         filePath = Path.Combine(Application.dataPath, "EyeTrackingData.txt");
         writer = new StreamWriter(filePath);
         writer.WriteLine("LeftEyeGazePosition,RightEyeGazePosition");
@@ -36,7 +50,17 @@ public class EyeDataCollection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        gazeDirection = Vector3.zero;
+        if (eyeTrackingDevice != null )
+        {
+            InputFeatureUsage<Vector3> eyeGazeDirectionUsage = new InputFeatureUsage<Vector3>("eyeGazeDirection");
+            if(eyeTrackingDevice.TryGetFeatureValue(eyeGazeDirectionUsage, out Vector3 gaze))
+            {
+                gazeDirection = gaze;
+                string dataToWrite = $"{gazeDirection}";
+                eyeData.Add(dataToWrite);
+            }
+        }
     }
  
    
